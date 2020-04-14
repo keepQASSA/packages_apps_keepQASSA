99681ce36cb2c69822bbeb34a4a450ca414fa632
simrat39 <simrats169169@gmail.com>
2020-04-14 09:31:09 -0700

    keepQASSA: Configurable QS Blur Radius [2/2]
    Change-Id: I790856436de4b6ecb402afd3d1e6c58fca765ab7
    Signed-off-by: ghou8s <ghou8s@gmail.com>

--- src/com/keepqassa/settings/fragments/quicksettings/BlurQuickSettings.java
+++ src/com/keepqassa/settings/fragments/quicksettings/BlurQuickSettings.java
@@ -27,6 +27,7 @@ import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.SwitchPreference;
 import android.provider.Settings;
 
+import com.keepqassa.settings.preferences.CustomSeekBarPreference;
 import com.keepqassa.settings.preferences.SystemSettingSwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto;
@@ -39,6 +40,10 @@ public class BlurQuickSettings extends SettingsPreferenceFragment implements
 
     public static final String TAG = "BlurQuickSettings";
 
+    private static final String QS_BLUR_RADIUS = "qs_blur_radius";
+
+    private CustomSeekBarPreference mQsBlurRadius;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,12 +52,24 @@ public class BlurQuickSettings extends SettingsPreferenceFragment implements
 
         PreferenceScreen prefScreen = getPreferenceScreen();
         ContentResolver resolver = getActivity().getContentResolver();
+
+        mQsBlurRadius = (CustomSeekBarPreference) findPreference(QS_BLUR_RADIUS);
+        final int blurRadius = Settings.System.getInt(resolver,
+                Settings.System.QS_BLUR_RADIUS, 0);
+            mQsBlurRadius.setValue((blurRadius));
+            mQsBlurRadius.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
         switch (preference.getKey()) {
+        case QS_BLUR_RADIUS:
+                Integer blurRadius = (Integer) newValue;
+                Settings.System.putInt(resolver,
+                        Settings.System.QS_BLUR_RADIUS, blurRadius);
+                return true;
+
             default:
                 return false;
         }

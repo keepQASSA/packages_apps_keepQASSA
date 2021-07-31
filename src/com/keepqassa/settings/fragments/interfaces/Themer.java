8cbd5b430567bbb01f00d914db671d4e3b5365e5
NurKeinNeid <mralexman3000@gmail.com>
2021-07-31 20:44:35 +0200

    keepQASSA: Move theming to keepQASSA [2/2]
    Change-Id: Icdb3b1e0534233995d5c0594feede6261033ec45
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

---
+++ src/com/keepqassa/settings/fragments/interfaces/Themer.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2020 DerpFest
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.keepqassa.settings.fragments.interfaces;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.os.SystemProperties;
+import androidx.preference.*;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.dashboard.DashboardFragment;
+import com.android.settings.display.OverlayCategoryPreferenceController;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
+
+import com.android.settings.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Themer extends DashboardFragment {
+    private static final String TAG = "Themer";
+
+    private ContentResolver mResolver;
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.QASSA;
+    }
+
+    @Override
+    protected String getLogTag() {
+        return TAG;
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    @Override
+    protected int getPreferenceScreenResId() {
+        return R.xml.themer;
+    }
+
+    @Override
+    protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
+        return buildPreferenceControllers(context, getSettingsLifecycle(), this);
+    }
+
+    private static List<AbstractPreferenceController> buildPreferenceControllers(
+            Context context, Lifecycle lifecycle, Fragment fragment) {
+        final List<AbstractPreferenceController> controllers = new ArrayList<>();
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.accent_color"));
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.font"));
+        controllers.add(new OverlayCategoryPreferenceController(context,
+                "android.theme.customization.adaptive_icon_shape"));
+        return controllers;
+    }
+}

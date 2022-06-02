6a9685f641a889d7fcb466f41f3ba039dae8ff0b
ghou8s <ghou8s@gmail.com>
2022-06-02 10:20:56 +0000

    keepQASSA: Add SystemUI restart for 'R style' & center "R style" notification headers
    Signed-off-by: ghou8s <ghou8s@gmail.com>

--- src/com/keepqassa/settings/fragments/Notifications.java
+++ src/com/keepqassa/settings/fragments/Notifications.java
@@ -16,6 +16,7 @@
 package com.keepqassa.settings.fragments;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
@@ -23,15 +24,29 @@ import android.os.Bundle;
 import android.provider.Settings;
 import androidx.preference.*;
 
-import com.android.internal.logging.nano.MetricsProto;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.SwitchPreference;
 
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.custom.ActionUtils;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+
+import com.keepqassa.settings.preferences.SystemSettingListPreference;
+import com.keepqassa.settings.preferences.SystemSettingSwitchPreference;
 
 public class Notifications extends SettingsPreferenceFragment
         implements Preference.OnPreferenceChangeListener {
 
     public static final String TAG = "Notifications";
+
+    private static final String NOTIFICATION_HEADERS  = "notification_headers";
+    private static final String CENTER_NOTIFICATION_HEADERS = "center_notification_headers";
+
+    private SystemSettingSwitchPreference mShowHeaders;
+    private SystemSettingSwitchPreference mCenterHeaders;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -39,11 +54,37 @@ public class Notifications extends SettingsPreferenceFragment
         addPreferencesFromResource(R.xml.keepqassa_settings_notifications);
         PreferenceScreen prefScreen = getPreferenceScreen();
         final Resources res = getResources();
+
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+        mShowHeaders = findPreference(NOTIFICATION_HEADERS);
+        mShowHeaders.setChecked((Settings.System.getInt(resolver,
+                Settings.System.NOTIFICATION_HEADERS, 0) == 1));
+        mShowHeaders.setOnPreferenceChangeListener(this);
+
+        mCenterHeaders = findPreference(CENTER_NOTIFICATION_HEADERS);
+        mCenterHeaders.setChecked((Settings.System.getInt(resolver,
+                Settings.System.CENTER_NOTIFICATION_HEADERS, 0) == 1));
+        mCenterHeaders.setOnPreferenceChangeListener(this);
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object objValue) {
-        return false;
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final ContentResolver resolver = getActivity().getContentResolver();
+	if (preference == mShowHeaders) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.NOTIFICATION_HEADERS, value ? 1 : 0);
+            ActionUtils.showSystemUiRestartDialog(getContext());
+            return true;
+	} else if (preference == mCenterHeaders) {
+            boolean value = (Boolean) newValue;
+            Settings.System.putInt(resolver,
+                    Settings.System.CENTER_NOTIFICATION_HEADERS, value ? 1 : 0);
+            ActionUtils.showSystemUiRestartDialog(getContext());
+            return true;
+        }
+        return true;
     }
 
     @Override
